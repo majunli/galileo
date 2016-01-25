09ea7d4ca7fa30acce80f6611e51e9e0e6a72b59
aguado <aguado@21cn.com>
2016-01-25 21:46:37 +0800

    添加两个宏： 1. DISALLOW_COPY_AND_ASSIGN 禁用复制 2. arraysize 计算数组大小

--- base/basetypes.h
+++ base/basetypes.h
@@ -3,6 +3,16 @@
 
 #include <cstdint>
 
+#undef DISALLOW_COPY_AND_ASSIGN
+// 限制编译器自动生成的类的拷贝构造函数和赋值函数
+// 注意：使用时应该声明为私有方法
+#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&);               \
+  void operator=(const TypeName&)
 
+template <typename T, size_t N>
+char(&ArraySizeHelper(T(&array)[N]))[N];
+// 正确计算数组中的元素数目
+#define arraysize(array) (sizeof(ArraySizeHelper(array)))
 
 #endif // !__GALILEO_BASE_BASETYPES_H__
